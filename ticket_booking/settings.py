5d850742ce025acbba424c19fe995e8805767289
Aakash Das <aakash.das@bankofloyal.com>
2019-04-25 16:06:22 +0530

    Modified whitenoise settings

--- ticket_booking/settings.py
+++ ticket_booking/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,4 +125,4 @@ STATICFILES_DIRS = [
 ]
 
 # white noise settings
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
